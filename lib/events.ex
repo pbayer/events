6f13fe156200388653bb37dbbe131b0a647e2dec
Paul Bayer <Paul.Bayer@gleichsam.de>
2021-05-02 19:51:18 +0200

    Initial commit of new project

---
+++ lib/events.ex
@@ -0,0 +1,18 @@
+defmodule Events do
+  @moduledoc """
+  Documentation for `Events`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Events.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
